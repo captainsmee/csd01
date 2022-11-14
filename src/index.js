fd6f99ef6c0d6c4e4f0e1fc9e70a7b88bf48703c
captainsmee <76639150+captainsmee@users.noreply.github.com>
2022-11-14 11:39:03 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>How are you?</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
